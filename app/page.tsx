e2e19aead921b5db1ae36286ec34bf8a2d2a33a7
xiliourt <dyl.sec.ure@gmail.com>
2025-08-07 22:24:13 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DomainTester from '@/components/DomainTester';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <main>
       <DomainTester />
     </main>
   );
